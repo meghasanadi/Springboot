5e91ad9830267d02cccb26da5d53dfe045b271fd
meghasanadi <meghakf@yahoo.com>
2018-05-23 15:49:13 +0530

    My initial commit message

--- src/main/java/com/samcomm/springboot/controller/RestControllerhelloWorld.java
+++ src/main/java/com/samcomm/springboot/controller/RestControllerhelloWorld.java
@@ -36,6 +36,12 @@ public class RestControllerhelloWorld {
 		
 	}
 	
+	@PostMapping(value="/create/again")
+	public Ticket createTicketanother(@RequestBody Ticket ticket) {
+		return ticketService.createTicket(ticket);
+		
+	}
+	
 	@GetMapping(value="/ticket/{ticketId}")
 	public Ticket getTicketById(@PathVariable("ticketId")Integer ticketId) {
 		
